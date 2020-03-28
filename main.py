d1611814a9dd0d1b74a48f48dc35e4ec3adbc184
davidleonardo11 <david.sepulveda.2020@upb.edu.co>
2020-03-27 20:56:39 -0500

    Mi primer commit

--- main.py
+++ main.py
@@ -6,11 +6,21 @@ Created on Fri Mar 27 19:50:52 2020
 """
 
 
-NumeroA = int(input("Ingrese numero1: "))
-NumeroC = int(input("Ingrese numero2: "))
+NumeroA = int(input("Ingrese numeroa: "))
+NumeroB = int(input("Ingrese numerob: "))
+NumeroC = int(input("Ingrese numeroc: "))
+NumeroD = int(input("Ingrese numerod: "))
+
+
 
 producto = (NumeroA*NumeroC)
 Doble = NumeroA*2
+Cuadrado = NumeroB**2
+Raiz = NumeroD**(1/2)
+
+
 
 print("El producto de los numeros es: ", producto)
 print("El doble es: ", Doble)
+print("El cuadrado del numero b es:", Cuadrado)
+print("La raiz del numero d es: ", Raiz)
