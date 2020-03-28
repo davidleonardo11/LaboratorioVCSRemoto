7d614def3520b5fc3eadd12dcb9f1058c8d29f48
davidleonardo11 <david.sepulveda.2020@upb.edu.co>
2020-03-27 20:27:53 -0500

    Mi primer commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 27 19:50:52 2020
+
+@author: DAVID LEONARDO
+"""
+
+
+NumeroA = int(input("Ingrese numero1: "))
+NumeroC = int(input("Ingrese numero2: "))
+
+producto = (NumeroA*NumeroC)
+Doble = NumeroA*2
+
+print("El producto de los numeros es: ", producto)
+print("El doble es: ", Doble)
